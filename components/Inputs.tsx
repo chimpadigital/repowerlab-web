cc889b061415b0bb9259293d670a248b2aa12a6e
Cintia j Martinez <93katu@gmail.com>
2024-09-18 16:42:55 -0300

    agregado react hook form

--- components/Inputs.tsx
+++ components/Inputs.tsx
@@ -1,43 +1,70 @@
+"use client";
+import { FormValues } from "@/app/about/contact-us/page";
 import React from "react";
+import { useController, UseControllerProps } from "react-hook-form";
 
-interface inputProps {
-  type?: "text" | "number" | "tel" | "email";
+interface InputProps extends UseControllerProps<FormValues> {
   placeholder: string;
+  type?: string; // opcional, por defecto puede ser "text",
+  error: string | undefined;
 }
 
-export const Input = ({ type = "text", placeholder }: inputProps) => {
+export const Input = (props: InputProps) => {
+  const { field } = useController(props);
   return (
-    <input
-      placeholder={placeholder}
-      type={type}
-      className="rounded-full focus-within:outline-2 border-white border-2 border-transparent focus:outline-none focus:border-accent placeholder-gray-400 transition-all duration-300  bg-[#C5C5C5]/15 py-4 px-8 backdrop-blur-2xl placeholder:text-white placeholder:font-medium"
-    />
+    <div className="flex flex-col w-full">
+      <input
+        placeholder={props.placeholder}
+        {...field}
+        type={props.type}
+        className="rounded-full focus-within:outline-2 border-white border-2 border-transparent focus:outline-none focus:border-accent placeholder-gray-400 transition-all duration-300  bg-[#C5C5C5]/15 py-4 px-8 backdrop-blur-2xl placeholder:text-white placeholder:font-medium"
+      />
+      <span
+        className={`text-red-500 mt-1 transition-all ${props.error ? " opacity-100 translate-x-0" : " opacity-0 -translate-x-2"}`}
+      >
+        {props.error && props.error}
+      </span>
+    </div>
   );
 };
 
-interface textAreaProps {
+interface textAreaProps extends UseControllerProps<FormValues> {
   placeholder: string;
+  error: string | undefined;
 }
 
-export const TextArea = ({ placeholder }: textAreaProps) => {
+export const TextArea = (props: textAreaProps) => {
+  const { field } = useController(props);
+
   return (
-    <textarea
-      placeholder={placeholder}
-      rows={6}
-      className="rounded-3xl focus-within:outline-2 border-white border-2 border-transparent focus:outline-none focus:border-accent placeholder-gray-400 transition-all duration-300  bg-[#C5C5C5]/15 py-4 px-8 backdrop-blur-2xl placeholder:text-white placeholder:font-medium"
-    />
+    <div className="flex flex-col w-full">
+      <textarea
+        {...field}
+        placeholder={props.placeholder}
+        rows={6}
+        className="rounded-3xl focus-within:outline-2 border-white border-2 border-transparent focus:outline-none focus:border-accent placeholder-gray-400 transition-all duration-300  bg-[#C5C5C5]/15 py-4 px-8 backdrop-blur-2xl placeholder:text-white placeholder:font-medium"
+      />
+      <span
+        className={`text-red-500 mt-1 transition-all ${props.error ? " opacity-100 translate-x-0" : " opacity-0 -translate-x-2"}`}
+      >
+        {props.error && props.error}
+      </span>
+    </div>
   );
 };
 
-interface radioButtonProps {
+interface radioButtonProps  extends UseControllerProps<FormValues>  {
   label: string;
+  value: "partner" | "client" | "talent";
 }
-export const RadioButton = ({ label }: radioButtonProps) => {
+export const RadioButton = (props: radioButtonProps) => {
+  const { field } = useController(props);
+
   return (
     <label className="w-fit items-center flex gap-[10px] cursor-pointer">
-      <input type="radio" className="hidden peer" />
+      <input {...field} type="radio" className="hidden peer" value={props.value}/>
       <div className="grid  place-items-center h-5 w-5 rounded-full border-2 border-white border-solid peer-checked:before:scale-100 before:w-3 before:absolute before:h-3 before:rounded-full before:bg-white before:scale-0 before:transition-all"></div>
-      <span className="text-white font-light">{label}</span>
+      <span className="text-white font-light">{props.label}</span>
     </label>
   );
 };
