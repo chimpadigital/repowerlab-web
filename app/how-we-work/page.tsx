efbea313ba9a1612c15e5527e9c296517d9df719
stefanoracke <stefanoracke@gmail.com>
2024-10-17 09:17:07 -0300

    footer arreglado

---
+++ app/how-we-work/page.tsx
@@ -0,0 +1,10 @@
+import Hero from '@/components/pages/how-we-work/Hero'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <Hero />
+        </div>
+    )
+}
---
+++ components/pages/how-we-work/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="w-full px-8 items-center flex-col w-full flex justify-center relative top-[-80px]">
+            <div className="w-full relative flex justify-center overflow-hidden h-full min-h-[90vh] max-h-[1500px] bg-primary rounded-[20px] pt-[100px]">
+                
+                <div className="container px-6 relative z-10 top-[-80px]">
+                    <div className="grid grid-cols-2 h-full">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
