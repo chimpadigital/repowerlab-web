95921dfaf84a1d487f512a19c40a00eae14694eb
stefanoracke <stefanoracke@gmail.com>
2024-10-25 09:26:47 -0300

    RL-14

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 export default function Hero() {
     return (
         <div className="w-full px-8 items-center flex-col w-full flex justify-center relative top-[-80px]">
-            <div className="w-full relative flex justify-center overflow-hidden h-full min-h-[90vh] max-h-[1500px]  rounded-[20px] pt-[100px]">
+            <div className="w-full relative flex justify-center overflow-hidden h-full min-h-[90vh] max-h-[1500px]  rounded-[20px] ">
                 <div className='object-cover bg-home w-full h-full top-0 left-0 absolute z-[1]'></div>
                 <video className='object-cover bg-secondary w-full h-full top-0 left-0 absolute z-0' autoPlay muted playsInline loop>
                     <source src="/video.mp4" type="video/mp4" />
                     Tu navegador no soporta la reproducción de videos.
                 </video>
-                <div className="container px-6 relative z-10 top-[-80px]">
+                <div className="container px-6 relative z-10">
                     <div className="grid grid-cols-2 h-full">
                         <div className="col-span-1 h-full flex flex-col justify-center">
                             <div className="max-w-[600px]">
