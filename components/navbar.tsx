5f64308513dffbfa4dec4942d54280a34deb7ca7
stefanoracke <stefanoracke@gmail.com>
2024-10-29 15:57:01 -0300

    RL-31

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,12 +32,23 @@ export const Navbar = () => {
       <div className="sticky top-[40px] mt-[20px] z-50 text-white w-full flex justify-center">
         <div className="container px-6">
           <div className="flex justify-between">
-            <div className="gap-3 max-w-fit py-4 px-6 relative  transition-all duration-[600ms] rounded-full" style={!isTop ? bgNavStyle : {}}>
+            {
+              active ?
+                <div className="gap-3 max-w-fit py-4 px-6 relative  transition-all duration-[600ms] rounded-full" >
 
-              <Link className="flex justify-start block  items-center gap-1" href="/">
-                <LogoRepower className={`${active ? 'text-primary' : 'text-white'} transition-all duration-[600]`} />
-              </Link>
-            </div>
+                  <Link className="flex justify-start block  items-center gap-1" href="/">
+                    <LogoRepower className={`${active ? 'text-primary' : 'text-white'} transition-all duration-[600]`} />
+                  </Link>
+                </div>
+
+                :
+                <div className="gap-3 max-w-fit py-4 px-6 relative  transition-all duration-[600ms] rounded-full" style={!isTop ? bgNavStyle : {}}>
+
+                  <Link className="flex justify-start block  items-center gap-1" href="/">
+                    <LogoRepower className={`${active ? 'text-primary' : 'text-white'} transition-all duration-[600]`} />
+                  </Link>
+                </div>
+            }
             <div className="flex justify-end items-center w-full gap-4">
 
               {
@@ -59,7 +70,7 @@ export const Navbar = () => {
         </div>
 
       </div>
-     
+
       <Menu active={active} setMenu={() => { setActive(!active) }}></Menu>
 
     </>
