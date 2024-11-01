984ae2b94af52d89274ff0ae04943bef2667cd3c
stefanoracke <stefanoracke@gmail.com>
2024-11-01 15:49:41 -0300

    scroll accordion

--- components/ScrollAccordion.tsx
+++ components/ScrollAccordion.tsx
@@ -65,7 +65,7 @@ export default function ScrollAccordion({
       <div className="container px-6">
         <div className="grid grid-cols-2 min-h-screen items-center sticky top-0 gap-[20px]">
           <div
-            className={`col-span-1 relative max-h-[700px]  max-[1366px]:max-h-[600px] max-[1536px]:max-h-[700px]    h-full ${imgPosition == "right" && "order-last"}`}
+            className={`col-span-1 relative max-h-[550px] min-[1400px]:max-h-[600px]   min-[1800px]:max-h-[700px]     h-full ${imgPosition == "right" && "order-last"}`}
           >
             {y &&
               items.map((el, index) => (
@@ -86,7 +86,7 @@ export default function ScrollAccordion({
             )}
           </div>
           <div
-            className={`col-span-1 max-h-[700px]  max-[1366px]:max-h-[600px] max-[1536px]:max-h-[700px]   p-12 shadow-lg rounded-[20px] h-full flex flex-col relative justify-between`}
+            className={`col-span-1 max-h-[550px] min-[1400px]:max-h-[600px] min-[1800px]:max-h-[700px]   p-12 shadow-lg rounded-[20px] h-full flex flex-col relative justify-between`}
           >
             <motion.div
               style={{
@@ -187,7 +187,7 @@ const AccordionMotion = ({ index, el, y, distancia }: any) => {
   return (
     <motion.div key={index} className={`relative`} style={{ y: top }}>
       <div className="py-2">
-        <div className="rounded-full w-full p-[20px] max-[1366px]:py-[10px] max-[1536px]:py-[20px] bg-grey-100 text-primary font-bold">
+        <div className="rounded-full w-full px-[20px] py-[12px]  bg-grey-100 text-primary font-bold">
           {el.title}
         </div>
       </div>
