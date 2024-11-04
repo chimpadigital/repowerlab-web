527c1c65c7370a6f3b51c460a81ba2ada93a1b79
stefanoracke <stefanoracke@gmail.com>
2024-11-04 10:03:02 -0300

    cambiando our numbers

--- components/pages/home/MapHome.tsx
+++ components/pages/home/MapHome.tsx
@@ -9,15 +9,15 @@ export default function MapHome() {
     <div className="w-full min-h-screen  px-8">
       <div className="relative w-full py-[80px] min-h-screen flex justify-center">
         <Image fill src={"/images/home/mapa-bg.png"} alt="" className='object-cover rounded-[20px]'></Image>
-        <div className="container relative">
+        <div className="container w-full relative">
           <div className="absolute z-10 bottom-12">
             <p className="text-[100px] font-light translate-x-[-150px] -translate-y-[200px] -rotate-90 text-white">Numbers</p>
           </div>
-          <p className="absolute z-10 bottom-[120px] left-[160px] text-[26px] w-[440px] text-white">Check out our track record, showcased through the numbers that define our success.</p>
+          <p className="absolute z-10 bottom-[120px] left-[160px] min-w-[1600px]:text-[18px] text-[16px] !w-[270px] text-white">Check out our track record, showcased through the numbers that define our success.</p>
           <div className="w-full flex relative h-full items-center">
-            <div className="flex justify-center items-center">
+            <div className="flex w-full justify-center items-center">
               <div className="w-[400px]"></div>
-              <div className="relative">
+              <div className="relative grow">
                 <Image width={1500} quality={100} height={900} src={"/images/home/mapa.png"} alt="" className=' w-full h-auto rounded-[20px]'></Image>
                 <Card top={"20%"} left={"7%"} title={"WIND TURBINES SOLD"} number={416}/>
                 <Card top={"60%"} left={"18%"} title={"FINISHED PROJECTS"} number={29}/>
