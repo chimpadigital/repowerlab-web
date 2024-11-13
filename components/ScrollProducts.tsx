89b8642f81f4c052854762593e4929e4830ab81c
jimena katu <93katu@gmail.com>
2024-11-13 10:44:49 -0300

    ahora si crep que esta scroll cards products

--- components/ScrollProducts.tsx
+++ components/ScrollProducts.tsx
@@ -54,7 +54,11 @@ export default function ScrollProducts() {
 
   return (
     <>
-      <div className="relative h-[200vh] md:h-[300vh]" ref={ref}>
+      <div
+        className="relative md:h-[300vh] pb-52"
+        ref={ref}
+        // style={{ height: "calc(100% + 28rem)" }}
+      >
         <div className="relative  top-0 z-0 w-full hidden md:block">
           <div className="sticky top-0  h-[100vh] z-[1]">
             <div className="absolute w-full h-full">
@@ -83,16 +87,23 @@ export default function ScrollProducts() {
             </div>
           </div>
         </div>
-        <div className="text-white px-4 absolute w-full h-full md:hidden pb-52">
-          <div className="sticky top-28 w-full h-fit z-[1]">
-            <CardMobile turbine={turbine} />
+
+        <div className="text-white px-4  top-0 w-full h-full md:hidden">
+          <div className="sticky top-28 w-full z-[1] h-fit">
+            <div className="relative w-full h-full  ">
+              <CardMobile turbine={turbine} />
+            </div>
           </div>
-          <div className="sticky top-32 w-full  h-fit z-[2]">
-            <CardMobile turbine={turbine1} top={110} />
+          <div className="sticky top-28 w-full z-[2] h-fit">
+            <div className="relative w-full h-full">
+              <CardMobile turbine={turbine1} top={110} />
+            </div>
           </div>
 
-          <div className="sticky top-36 w-full  h-fit z-[3]">
-            <CardMobile turbine={turbine2} top={220} />
+          <div className="sticky top-80 w-full z-[3] h-fit">
+            <div className="relative w-full h-full">
+              <CardMobile turbine={turbine2} top={220} />
+            </div>
           </div>
         </div>
       </div>
