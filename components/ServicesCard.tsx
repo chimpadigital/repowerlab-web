3848a89b72078b36791baa8fb3bbebc33b925892
jimena katu <93katu@gmail.com>
2024-11-14 15:05:39 -0300

    lkisto slider de servicios

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
-import { Arrow, GruaIcono } from "./icons";
+import {
+  Arrow,
+  ArrowButtonIcon,
+  ArrowDiagonal,
+  ArrowMenu,
+  ArrowRight,
+  GruaIcono,
+} from "./icons";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
@@ -8,6 +15,7 @@ interface ServicesCardProps {
   titulo: string;
   svgIcono: React.ReactNode;
   texto: string;
+  textoMobile?: string;
   link: string;
 }
 
@@ -19,28 +27,41 @@ const item = {
   },
 };
 
-const ServicesCard = ({ titulo, svgIcono, texto, link }: ServicesCardProps) => {
-
+const ServicesCard = ({
+  titulo,
+  svgIcono,
+  texto,
+  link,
+  textoMobile,
+}: ServicesCardProps) => {
   return (
     <motion.div
       variants={item}
-      className="relative overflow-hidden group shadow-[5px_5px_4px_0px_#0000001A] rounded-[10px] bg-[#F4F4F4] w-fit text-primary min-h-[280px] px-6 pb-6"
+      className="relative overflow-hidden group shadow-[5px_5px_4px_0px_#0000001A] rounded-[10px] bg-[#F4F4F4] w-fit text-primary md:min-h-[280px] px-6 pb-6 h-full"
       style={{
         width: "min(100%, 307px)",
       }}
     >
-      <div className="h-full transition-all group-hover:-translate-y-8  w-full flex gap-4 flex-col pt-14">
-        <div className="bg-[#B3C5DF] h-[78px] aspect-square rounded-full grid place-items-center w-fit">
-          {svgIcono}
+      <div className="h-full transition-all group-hover:-translate-y-8 flex  flex-col justify-between  w-full pt-14">
+        <div className="flex gap-4 flex-col">
+          <div className="bg-[#B3C5DF] h-[78px] aspect-square rounded-full grid place-items-center w-fit">
+            {svgIcono}
+          </div>
+          <h4
+            className={`${titulo.length < 16 ? "max-w-[9ch]" : "max-w-[13ch] md:max-w-[12ch]"}  text-[22px] md:text-xl md:font-light`}
+          >
+            {titulo}
+          </h4>
         </div>
-        <h4
-          className={`${titulo.length < 16 ? "max-w-[9ch]" : "max-w-[12ch]"}  text-xl font-light`}
-        >
-          {titulo}
-        </h4>
+        <div className="stroke-primary w-4 self-end md:hidden">
+          <ArrowDiagonal />
+        </div>
       </div>
       <div className="absolute p-[26px] inset-0 translate-y-[100%] transition-all group-hover:translate-y-0 bg-[#F4F4F4] h-full w-full flex flex-col justify-between">
-        <p className="text-base">{texto}</p>
+        <p className={`text-base ${textoMobile ? "hidden md:block" : "block"}`}>
+          {texto}
+        </p>
+        {textoMobile && <p className="text-base md:hidden">{textoMobile}</p>}
         <Link
           href={link}
           className=" flex items-center gap-2 ml-auto font-semibold"
