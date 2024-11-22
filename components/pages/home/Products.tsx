ab000f448013f604efcd6ff977d6742f8afdc615
stefanoracke <stefanoracke@gmail.com>
2024-11-22 11:21:11 -0300

    links products

--- components/pages/home/Products.tsx
+++ components/pages/home/Products.tsx
@@ -60,7 +60,7 @@ export default function Products() {
               </div>
             </div>
             <div className="py-4 mt-4 flex justify-center w-full">
-              <Link href="products/wind-turbines" className='w-full mt-6 max-w-[180px] relative overflow-hidden gap-4 items-center transition-all duration-[600] py-2 px-6 rounded-full  text-white flex justify-between hover:max-w-[190px]'>
+              <Link href="/products/wind-turbines" className='w-full mt-6 max-w-[180px] relative overflow-hidden gap-4 items-center transition-all duration-[600] py-2 px-6 rounded-full  text-white flex justify-between hover:max-w-[190px]'>
                 <div className="w-full absolute h-full top-0 left-0 bg-primary">
                 </div>
                 <span className='relative text-[18px]'>{t('read')}</span>
@@ -75,7 +75,7 @@ export default function Products() {
           <div className="container px-4 pt-8">
             <ProductsSwiper />
             <div className="py-4 mt-4 flex justify-center w-full">
-              <Link href="products/wind-turbines" className='w-full mt-6 max-w-[180px] relative overflow-hidden gap-4 items-center transition-all duration-[600] py-2 px-6 rounded-full  text-white flex justify-between hover:max-w-[190px]'>
+              <Link href="/products/wind-turbines" className='w-full mt-6 max-w-[180px] relative overflow-hidden gap-4 items-center transition-all duration-[600] py-2 px-6 rounded-full  text-white flex justify-between hover:max-w-[190px]'>
                 <div className="w-full absolute h-full top-0 left-0 bg-primary">
                 </div>
                 <span className='relative text-[18px]'>{t('read')}</span>
