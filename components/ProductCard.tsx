c62212d0171e490b1860e4db24229abbb2bb3a94
stefanoracke <stefanoracke@gmail.com>
2024-11-22 11:20:04 -0300

    product card min-w

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -40,7 +40,7 @@ export default function ProductCard({ title, img, height, href, description }: {
                         <div className='text-[24px] font-normal relative pe-4 flex' >
                             <div className="flex w-full justify-between">
                                 <Title className='' title={title} />
-                                <motion.div variants={variantFlecha} transition={{ duration: 0.6 }} animate={open ? "open" : "closed"} className="w-12 h-12 bg-[#C5C5C526] right-0 relative z-10 text-white rounded-full border-white border cursor-pointer" style={{}}>
+                                <motion.div variants={variantFlecha} transition={{ duration: 0.6 }} animate={open ? "open" : "closed"} className="w-12 h-12 min-w-12 min-h-12 bg-[#C5C5C526] right-0 relative z-10 text-white rounded-full border-white border cursor-pointer" style={{}}>
                                     <div className="flex h-full w-full justify-center items-center">
                                         <div className="h-3 fill-white left-px relative">
                                             <ArrowRight />
