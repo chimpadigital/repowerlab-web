6dbc81c9f93cee0c6c9f0d3be2a539cfda24977a
stefanoracke <stefanoracke@gmail.com>
2024-11-25 16:17:19 -0300

    arreglos

--- components/pages/products/Support.tsx
+++ components/pages/products/Support.tsx
@@ -13,9 +13,9 @@ export default function Support() {
       <h2
         className={
           title({ color: "primary" }) +
-          " md:text-center  max-w-[12ch] md:w-full px-6 md:px-0 leading-7 md:leading-normal"
+          " md:text-center  max-w-[12ch] md:max-w-[100%] md:w-full px-6 md:px-0 leading-7 md:leading-normal"
         }
-      >
+      > 
         {t("title")}
       </h2>
       <p className="pb-12 pt-4 md:pt-8 max-w-[600px] text-grey-parrafo md:text-center px-6 md:px-0">
