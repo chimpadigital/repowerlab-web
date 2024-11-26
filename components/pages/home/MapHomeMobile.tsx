5679052a24f6d8481e86bb89a2f56b7a4784eb5a
stefanoracke <stefanoracke@gmail.com>
2024-11-26 17:36:58 -0300

    Mapa Mobile traducciones

--- components/pages/home/MapHomeMobile.tsx
+++ components/pages/home/MapHomeMobile.tsx
@@ -1,17 +1,19 @@
 "use client"
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 export default function MapHomeMobile() {
+    const t = useTranslations('Home.Numbers')
     return (
         <div className=' bg-[url(/images/home/mapa-mob.png)] bg-no-repeat px-6 py-[40px] bg-cover bg-center rounded-[20px]'>
-            <h4 className='text-[22px] text-white font-bold'>Numbers</h4>
-            <p className="pt-2 text-[18px] text-white">Check out our track record, showcased through the numbers that define our success.</p>
+            <h4 className='text-[22px] text-white font-bold'>{t('title')}</h4>
+            <p className="pt-2 text-[18px] text-white">{t('subtitle')}</p>
             <div className="flex justify-center items-center pt-12 flex-col gap-4">
 
-                <CardM title={"WIND TURBINES SOLD"} number={416} />
-                <CardM title={"FINISHED PROJECTS"} number={29} />
-                <CardM title={"SATISFIED CLIENTS"} number={45} />
-                <CardM title={"DISMANTLED WIND FARMS"} number={16} />
+                <CardM title={t.raw('card1')} number={416} />
+                <CardM title={t.raw('card2')} number={29} />
+                <CardM title={t.raw('card3')} number={45} />
+                <CardM title={t.raw('card4')} number={16} />
             </div>
         </div>
     )
