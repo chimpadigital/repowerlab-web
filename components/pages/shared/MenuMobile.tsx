cdff061477fc23866504f81df57725846efd474b
jimena katu <93katu@gmail.com>
2024-12-04 14:31:39 -0300

    agregado link a logo en menu mobile

--- components/pages/shared/MenuMobile.tsx
+++ components/pages/shared/MenuMobile.tsx
@@ -22,7 +22,9 @@ export default function MenuMobile({ open, setOpen }: { open: boolean, setOpen?:
         <div className="fixed w-screen md:hidden left-0 top-0 z-[1000] block h-screen" style={{ transform: open ? 'translateX(0%)' : 'translateX(100%)', transition: "1500ms" }}>
             <Image fill quality={100} src="/images/shared/menudesplegable.png" alt='menu' className='absolute object-cover top-0 bg-secondary w-full z-10'></Image>
             <div className="relative text-primary flex justify-between py-8 px-6 z-10 items-center">
-                <LogoRepower />
+                <Link href={'/'}>
+                    <LogoRepower />
+                </Link>
                 <CloseIcon onClick={() => { setOpen(false) }} />
             </div>
             <div style={{ height: "calc(100dvh - 130px)" }} className='overflow-y-scroll'>
