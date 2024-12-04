7634cf6c83a488438175cd39b43dfbe8f296e9c7
jimena katu <93katu@gmail.com>
2024-12-04 10:55:28 -0300

    sumado idDekstop a componentes faltantes

--- components/pages/circular-economy/SolutionsAccordeon.tsx
+++ components/pages/circular-economy/SolutionsAccordeon.tsx
@@ -13,18 +13,21 @@ const SolutionsAccordeon = () => {
       <ScrollAccordion
         items={repoweringSolutions}
         id="repowering-solutions"
+        idDesktop="repowering-solutions-desk"
         imgPosition="right"
         imgText="CircularEconomy.SolutionsCards.imgText1"
       />
       <ScrollAccordion
         items={circularEconomySolutions}
         id="circular-economy-solution"
+        idDesktop="circular-economy-solution-desk"
         titleItems="CircularEconomy.SolutionsCards.itemTitle2"
         imgText="CircularEconomy.SolutionsCards.imgText2"
       />
       <ScrollAccordion
         items={refurbishmentSolutions}
         id="refurbishment"
+        idDesktop="refurbishment-desk"
         titleItems="CircularEconomy.SolutionsCards.itemTitle3"
         imgText="CircularEconomy.SolutionsCards.imgText3"
         imgPosition="right"
@@ -32,7 +35,8 @@ const SolutionsAccordeon = () => {
       <ScrollAccordion
         items={recyclingSolutions}
         id="recycling-solution"
-       titleItems="CircularEconomy.SolutionsCards.itemTitle4"
+        idDesktop="recycling-solution-desk"
+        titleItems="CircularEconomy.SolutionsCards.itemTitle4"
         imgText="CircularEconomy.SolutionsCards.imgText4"
       />
     </section>
